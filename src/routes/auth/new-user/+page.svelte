67eed826bf49943ef62ad582af2e23a6dbc004bb
tbarkas <tbarkas@cognity.gr>
2023-04-29 15:06:32 +0300

    Add stepper as an example to showcase the process of adding a new-user

--- src/routes/auth/new-user/+page.svelte
+++ src/routes/auth/new-user/+page.svelte
@@ -1,27 +1,34 @@
 <script>
-    import {signOut} from "@auth/sveltekit/client";
-    import {page} from "$app/stores";
-    export let data;
-    const {roles,meta}=data;
+	import { signOut } from '@auth/sveltekit/client';
+	import { page } from '$app/stores';
+	import { Step, Stepper } from '@skeletonlabs/skeleton';
+	import UserStepper from './UserStepper.svelte';
+	export let data;
+	const { roles, meta } = data;
 </script>
 
 <h1>New User page</h1>
-<p>Create forms to collect user data,add data to database and update jwt roles and meta data if needed!</p>
+<p>
+	Create forms to collect user data,add data to database and update jwt roles and meta data if
+	needed!
+</p>
+
+<UserStepper />
+
+<hr class="mt-10 mb-10 !border-t-8" />
+
 {#if $page.data.session}
-    {#if $page.data.session.user?.image}
-        <span
-                style="background-image: url('{$page.data.session.user.image}')"
-                class="avatar"
-        />
-    {/if}
-    <span class="signedInText">
-        <small>Signed in as</small><br/>
-        <strong>{$page.data.session.user?.name ?? "User"}</strong>
-      </span>
-    <div>
-        {#if roles}
-            {roles}
-        {/if}
-    </div>
-    <button on:click={() => signOut()} class="button">Sign out</button>
+	{#if $page.data.session.user?.image}
+		<span style="background-image: url('{$page.data.session.user.image}')" class="avatar" />
+	{/if}
+	<span class="signedInText">
+		<small>Signed in as</small><br />
+		<strong>{$page.data.session.user?.name ?? 'User'}</strong>
+	</span>
+	<div>
+		{#if roles}
+			{roles}
+		{/if}
+	</div>
+	<button on:click={() => signOut()} class="button">Sign out</button>
 {/if}
---
+++ src/routes/auth/new-user/UserStepper.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Step, Stepper } from '@skeletonlabs/skeleton';
+	function onCompleteHandler(e: Event): void {
+		console.log('event:complete', e);
+	}
+</script>
+
+<Stepper on:complete={onCompleteHandler}>
+	<Step>
+		<svelte:fragment slot="header">The user is authorized!</svelte:fragment>
+		The user is authorized but is not yet inserted on the database.
+	</Step>
+	<Step>
+		<svelte:fragment slot="header">Be aware of next steps!</svelte:fragment>
+		We are going to complete a stepper asking you for all important User meta data and then we going
+		to insert this User on the Database.
+	</Step>
+	<Step>
+		<svelte:fragment slot="header">Add User Data</svelte:fragment>
+		<p>...</p>
+		<p>...</p>
+		<p>After you add all user Data you can submit complete and the user will be created</p>
+	</Step>
+</Stepper>
